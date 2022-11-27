d75e296a57198133e4ea7bf9ddb8e81ebf88fe49
Matvey <morozovmatvej75@gmail.com>
2022-11-27 20:35:17 +0300

    add UI (ID can be now generated by user)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function generateID(len = 16) {
+function getID(len) {
     // UTF-16 codes of all english letters in capital case (A-Z)
     const charCodes = Array.from(Array(26), (el, index) => index + 65);
     // actual letters from character codes + dash ("-")
@@ -9,6 +9,7 @@ function generateID(len = 16) {
 
     let id = ""
 
+    // generate a random letter A-Z. 50% chance of it being lowercase or uppercase
     function randomLetter() {
         if (Math.random() < 0.5) {
             return alphabet[Math.floor(Math.random() * alphabet.length)];
@@ -18,14 +19,17 @@ function generateID(len = 16) {
         }
     }
 
+    // generate a random number 0-9
     function randomNumber() {
         return numbers[Math.floor(Math.random() * numbers.length)]
     }
 
     for (let i = 0; i < len; i++) {
+        // if previous character was a number, generate a string
         if (!isNaN(id[id.length - 1])) {
             id += randomLetter();
         }
+        // 50% chance of generating either a string or a number
         else if (Math.random() < 0.5) {
             id += randomLetter();
         }
@@ -36,4 +40,10 @@ function generateID(len = 16) {
     return id
 }
 
-console.log(generateID(20));
+function generateID() {
+    const input = document.querySelector(".chars-amount-input")
+    // if no integer is provided, default to 16
+    const length = input.value.length === 0 ? 16 : input.value;
+    const ID = getID(length);
+    document.querySelector(".generated-string").textContent = ID;
+}
